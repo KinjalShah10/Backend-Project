4b00d9c35aa5f5e71e3bda7ce3d4bff444622c5d
KinjalShah10 <shahkinjal0813@gmail.com>
2024-06-25 15:45:21 +0530

    login and logout added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -142,6 +142,51 @@ const isPasswordValid = await user.isPasswordCorrect (password)
 const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
 
 //6
-User.findById(user._id)
+const loggedInUser = await User.findById(user._id).
+select("-password -refreshToken")
+    
+const options = {
+    httpOnly: true,
+    secure:true
+
+}
+return res.
+status(200).
+cookie("accessToken",accessToken,options).
+cookie("refreshToken",refreshToken,options)
+.json(
+    new ApiResponse(
+        200,
+        {
+          user:loggedInUser,accessToken,
+          refreshToken  
+        },
+        "User logged In Successfully....."
+    )
+)
 })
-export {registerUser,loginUser}
+
+const logoutUser = asyncHandler(async(req,res)=>{
+   await User.findByIdAndUpdate(
+        req.user._id,{
+            $set:{
+                refreshToken : undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200, {}, "User Logged Out Successfully."))
+
+
+}) 
+export {registerUser,loginUser,logoutUser}
